9d494e75127053063e587731c1fa24d2c0834e9f
Carl Eriksson <carl.erikssonn@hotmail.com>
2024-04-20 14:15:40 +0200

    Add dynamic player spawning

--- Assets/Scripts/RoomGenerationController.cs
+++ Assets/Scripts/RoomGenerationController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Reflection;
 using UnityEditor;
+using UnityEditor.UIElements;
+using Logger = erikssonn.Logger;
 using Random = UnityEngine.Random;
 
 [CustomEditor(typeof(RoomGenerationController))]
@@ -14,16 +16,23 @@ public class RoomGenerationControllerEditor : Editor {
         if (GUILayout.Button("GENERATE")) {
             gc.Generate();
         }
+
         if (GUILayout.Button("CLEAR")) {
             gc.Clear();
         }
+
         EditorUtility.SetDirty(gc);
     }
 }
 
 [System.Serializable]
 public class Tile {
-    public enum TileType { NONE, CORRIDOR, ROOM, DOORWAY };
+    public enum TileType {
+        NONE,
+        CORRIDOR,
+        ROOM,
+        DOORWAY
+    };
 
     public int taken;
     public GameObject obj;
@@ -42,40 +51,49 @@ public class RoomGenerationController : MonoBehaviour {
     [Header("ASSIGNABLE: ")]
     [Tooltip("Tile which the generator uses for its mesh generation. This can be any prefab, it doesn't have to be a basic plane.")]
     [SerializeField] private GameObject tile = null;
+
     [Tooltip("Material that the generator assigns to the created mesh")]
     [SerializeField] private Material mat = null;
+
     [Tooltip("Player object")]
     [SerializeField] private GameObject player = null;
 
     [Header("SETTINGS: ")]
     [Tooltip("Size of the generated dungeon in tiles for each axis. a map size of 16 creates a dungeon of 16x16")]
     [SerializeField] private int mapSize = 16;
+
     [Tooltip("Amount of rooms the generator aims at generating. A high amount means more open areas and rooms being generated")]
     [SerializeField] private int roomGoal = 8;
+
     [Tooltip("Smooths the generation, a higher value is slower but removes unwanted parts of the dungeon")]
     [SerializeField] private int smoothness = 2;
+
     [Tooltip("Max size of the generated rooms. It uses the following algorithm: 'roomSize = 1 + (value * 2)', where value is random between 0 and maxRoomRadius")]
     [SerializeField] private int maxRoomRadius = 2;
+
     [Tooltip("Scales the whole mesh of the dungeon")]
     [SerializeField] private float meshScaling = 3;
+
     [Tooltip("Scales the height of the walls in the dungeon")]
     [SerializeField] private float dungeonHeight = 1;
 
     [Header("DEBUG: ")]
     [Tooltip("Generate the dungeon on game start")]
     [SerializeField] private bool generateOnStart = false;
+
     [Tooltip("Draw gizmos of the generated dungeon in the scene-view, where each piece of the dungeon is shown as a color")]
     [SerializeField] private bool drawGizmos = false;
 
     private Tile[,] grid = null;
     private int retries = 0;
+    private List<Vector3> eligibleSpawnPositions = new List<Vector3>();
 
     public int GetMapSize() => mapSize;
-    
+
     private void OnDrawGizmos() {
         if (drawGizmos) {
             Gizmos.DrawCube(transform.position + new Vector3(Mathf.RoundToInt(mapSize / 2) - 0.5f,
-                -1, Mathf.RoundToInt(mapSize / 2) - 0.5f) * meshScaling, new Vector3(mapSize, 0, mapSize)  * meshScaling);
+                -1, Mathf.RoundToInt(mapSize / 2) - 0.5f) * meshScaling, new Vector3(mapSize, 0, mapSize) * meshScaling);
 
             if (grid != null && grid.GetLength(0) > 0) {
                 for (int x = 0; x < grid.GetLength(0); x++) {
@@ -88,11 +106,19 @@ public class RoomGenerationController : MonoBehaviour {
                     }
                 }
             }
+
+            // if (eligibleSpawnPositions.Count <= 0) {
+            //     return;
+            // }
+            // Gizmos.color = Color.magenta;
+            // for (int i = 0; i < eligibleSpawnPositions.Count; i++) {
+            //     Gizmos.DrawSphere(eligibleSpawnPositions[i], 0.1f);
+            // }
         }
     }
 
     private void Start() {
-        if(generateOnStart) Generate();
+        if (generateOnStart) Generate();
     }
 
     public void Generate() {
@@ -114,9 +140,11 @@ public class RoomGenerationController : MonoBehaviour {
         GenerateWallMesh();
         MergeMesh();
         ScaleMap(meshScaling);
+        SpawnPlayer();
     }
 
     public void Clear() {
+        eligibleSpawnPositions.Clear();
         highestSection = 0;
         if (transform.childCount > 0) {
             for (int i = transform.childCount - 1; i >= 0; i--) {
@@ -124,6 +152,7 @@ public class RoomGenerationController : MonoBehaviour {
                 DestroyImmediate(child.gameObject);
             }
         }
+
         ClearLog();
     }
 
@@ -142,6 +171,10 @@ public class RoomGenerationController : MonoBehaviour {
                 grid[x, y] = new Tile(0, null, Tile.TileType.NONE, false);
             }
         }
+    }
+
+    private void SpawnPlayer() {
+        player.transform.position = eligibleSpawnPositions[Random.Range(0, eligibleSpawnPositions.Count - 1)] + new Vector3(0, 1, 0);
     }
 
     private void MergeMesh() {
@@ -278,6 +311,7 @@ public class RoomGenerationController : MonoBehaviour {
                     triangles.Add(startIndex + 3);
                     triangles.Add(startIndex + 2);
                 }
+
                 if (x + 1 >= grid.GetLength(0)) {
                     Vector3 topLeft = new Vector3(x + 1, dungeonHeight, y + 1);
                     Vector3 topRight = new Vector3(x + 1, dungeonHeight, y);
@@ -295,6 +329,7 @@ public class RoomGenerationController : MonoBehaviour {
                     triangles.Add(startIndex + 3);
                     triangles.Add(startIndex + 2);
                 }
+
                 if (y - 1 < 0) {
                     Vector3 topLeft = new Vector3(x + 1, dungeonHeight, y);
                     Vector3 topRight = new Vector3(x, dungeonHeight, y);
@@ -312,6 +347,7 @@ public class RoomGenerationController : MonoBehaviour {
                     triangles.Add(startIndex + 3);
                     triangles.Add(startIndex + 2);
                 }
+
                 if (x - 1 < 0) {
                     Vector3 topLeft = new Vector3(x, dungeonHeight, y);
                     Vector3 topRight = new Vector3(x, dungeonHeight, y + 1);
@@ -357,7 +393,7 @@ public class RoomGenerationController : MonoBehaviour {
             }
         }
 
-        if(normals.Count < vertices.Count) {
+        if (normals.Count < vertices.Count) {
             for (int i = normals.Count; i < vertices.Count; i++) {
                 normals.Add(Vector3.zero);
             }
@@ -385,6 +421,7 @@ public class RoomGenerationController : MonoBehaviour {
                 if (grid[x, y].taken != 1) continue;
                 GameObject newTile = Instantiate(tile, transform, false) as GameObject;
                 newTile.transform.position = new Vector3(x, 0, y);
+                eligibleSpawnPositions.Add(new Vector3(x, 0, y));
                 grid[x, y].obj = newTile;
             }
         }
@@ -407,10 +444,12 @@ public class RoomGenerationController : MonoBehaviour {
                 canPlace = false;
                 break;
             }
+
             if (!canPlace) {
                 break;
             }
         }
+
         if (canPlace) {
             for (int x = pos.x - roomSize; x <= pos.x + roomSize; x++) {
                 for (int y = pos.y - roomSize; y <= pos.y + roomSize; y++) {
@@ -419,11 +458,13 @@ public class RoomGenerationController : MonoBehaviour {
                 }
             }
         }
+
         retries++;
         if (retries < 10) {
             GenerateRoom();
             return;
         }
+
         retries = 0;
     }
 
@@ -434,20 +475,23 @@ public class RoomGenerationController : MonoBehaviour {
     private int NeighbourCount(Vector2Int pos) {
         int amount = 0;
 
-        if(pos.x - 1 < 0 || pos.x + 1 >= grid.GetLength(0) ||
-                pos.y - 1 < 0 || pos.y + 1 >= grid.GetLength(1)) {
+        if (pos.x - 1 < 0 || pos.x + 1 >= grid.GetLength(0) ||
+            pos.y - 1 < 0 || pos.y + 1 >= grid.GetLength(1)) {
             return 4;
         }
 
         if (grid[pos.x - 1, pos.y].taken == 1) {
             amount++;
         }
+
         if (grid[pos.x + 1, pos.y].taken == 1) {
             amount++;
         }
+
         if (grid[pos.x, pos.y - 1].taken == 1) {
             amount++;
         }
+
         if (grid[pos.x, pos.y + 1].taken == 1) {
             amount++;
         }
@@ -459,19 +503,22 @@ public class RoomGenerationController : MonoBehaviour {
         int amount = 0;
 
         if (pos.x - 1 < 0 || pos.x + 1 >= visited.GetLength(0) ||
-                pos.y - 1 < 0 || pos.y + 1 >= visited.GetLength(1)) {
+            pos.y - 1 < 0 || pos.y + 1 >= visited.GetLength(1)) {
             return 4;
         }
 
         if (visited[pos.x - 1, pos.y] == true) {
             amount++;
         }
+
         if (visited[pos.x + 1, pos.y] == true) {
             amount++;
         }
+
         if (visited[pos.x, pos.y - 1] == true) {
             amount++;
         }
+
         if (visited[pos.x, pos.y + 1] == true) {
             amount++;
         }
@@ -482,7 +529,7 @@ public class RoomGenerationController : MonoBehaviour {
     private void GenerateCorridors() {
         for (int x = 1; x < mapSize - 1; x++) {
             for (int y = 1; y < mapSize - 1; y++) {
-                if(grid[x, y].taken == 0 && NeighbourCount(new Vector2Int(x, y)) == 0) {
+                if (grid[x, y].taken == 0 && NeighbourCount(new Vector2Int(x, y)) == 0) {
                     GenerateCorridor(new Vector2Int(x, y));
                 }
             }
@@ -508,7 +555,7 @@ public class RoomGenerationController : MonoBehaviour {
                 continue;
             }
 
-            if(NeighbourCountVisited(new Vector2Int(pos.x, pos.y), visited) > 1) {
+            if (NeighbourCountVisited(new Vector2Int(pos.x, pos.y), visited) > 1) {
                 continue;
             }
 
@@ -518,20 +565,23 @@ public class RoomGenerationController : MonoBehaviour {
                 break;
             }
 
-            if (grid[pos.x - 1, pos.y].taken == 0 && NeighbourCount(new Vector2Int(pos.x - 1, pos.y)) == 0 && 
-                    NeighbourCountVisited(new Vector2Int(pos.x - 1, pos.y), visited) <= 1) {
+            if (grid[pos.x - 1, pos.y].taken == 0 && NeighbourCount(new Vector2Int(pos.x - 1, pos.y)) == 0 &&
+                NeighbourCountVisited(new Vector2Int(pos.x - 1, pos.y), visited) <= 1) {
                 stack.Push(new Vector2Int(pos.x - 1, pos.y));
             }
-            if (grid[pos.x + 1, pos.y].taken == 0 && NeighbourCount(new Vector2Int(pos.x + 1, pos.y)) == 0 && 
-                    NeighbourCountVisited(new Vector2Int(pos.x + 1, pos.y), visited) <= 1) {
+
+            if (grid[pos.x + 1, pos.y].taken == 0 && NeighbourCount(new Vector2Int(pos.x + 1, pos.y)) == 0 &&
+                NeighbourCountVisited(new Vector2Int(pos.x + 1, pos.y), visited) <= 1) {
                 stack.Push(new Vector2Int(pos.x + 1, pos.y));
             }
-            if (grid[pos.x, pos.y - 1].taken == 0 && NeighbourCount(new Vector2Int(pos.x, pos.y - 1)) == 0 && 
-                    NeighbourCountVisited(new Vector2Int(pos.x, pos.y - 1), visited) <= 1) {
+
+            if (grid[pos.x, pos.y - 1].taken == 0 && NeighbourCount(new Vector2Int(pos.x, pos.y - 1)) == 0 &&
+                NeighbourCountVisited(new Vector2Int(pos.x, pos.y - 1), visited) <= 1) {
                 stack.Push(new Vector2Int(pos.x, pos.y - 1));
             }
-            if (grid[pos.x, pos.y + 1].taken == 0 && NeighbourCount(new Vector2Int(pos.x, pos.y + 1)) == 0 && 
-                    NeighbourCountVisited(new Vector2Int(pos.x, pos.y + 1), visited) <= 1) {
+
+            if (grid[pos.x, pos.y + 1].taken == 0 && NeighbourCount(new Vector2Int(pos.x, pos.y + 1)) == 0 &&
+                NeighbourCountVisited(new Vector2Int(pos.x, pos.y + 1), visited) <= 1) {
                 stack.Push(new Vector2Int(pos.x, pos.y + 1));
             }
         }
@@ -551,14 +601,14 @@ public class RoomGenerationController : MonoBehaviour {
         for (int x = 0; x < grid.GetLength(0); x++) {
             for (int y = 0; y < grid.GetLength(1); y++) {
                 if (x - 1 < 0 || x + 1 >= grid.GetLength(0) ||
-                        y - 1 < 0 || y + 1 >= grid.GetLength(1)) {
+                    y - 1 < 0 || y + 1 >= grid.GetLength(1)) {
                     continue;
                 }
 
-                if (grid[x, y].taken == 0 && (((grid[x-1, y].taken == 1 && grid[x - 1, y].tileType != Tile.TileType.DOORWAY) &&
-                        (grid[x + 1, y].taken == 1 && grid[x + 1, y].tileType != Tile.TileType.DOORWAY)) ||
-                        ((grid[x, y - 1].taken == 1 && grid[x, y - 1].tileType != Tile.TileType.DOORWAY) &&
-                        (grid[x, y + 1].taken == 1 && grid[x, y + 1].tileType != Tile.TileType.DOORWAY))) && NeighbourCount(new Vector2Int(x, y)) == 2) {
+                if (grid[x, y].taken == 0 && (((grid[x - 1, y].taken == 1 && grid[x - 1, y].tileType != Tile.TileType.DOORWAY) &&
+                                               (grid[x + 1, y].taken == 1 && grid[x + 1, y].tileType != Tile.TileType.DOORWAY)) ||
+                                              ((grid[x, y - 1].taken == 1 && grid[x, y - 1].tileType != Tile.TileType.DOORWAY) &&
+                                               (grid[x, y + 1].taken == 1 && grid[x, y + 1].tileType != Tile.TileType.DOORWAY))) && NeighbourCount(new Vector2Int(x, y)) == 2) {
                     grid[x, y].taken = 1;
                     grid[x, y].tileType = Tile.TileType.DOORWAY;
                 }
@@ -580,7 +630,7 @@ public class RoomGenerationController : MonoBehaviour {
             for (int x = 0; x < grid.GetLength(0); x++) {
                 for (int y = 0; y < grid.GetLength(1); y++) {
                     if (x - 1 < 0 || x + 1 >= grid.GetLength(0) ||
-                            y - 1 < 0 || y + 1 >= grid.GetLength(1)) {
+                        y - 1 < 0 || y + 1 >= grid.GetLength(1)) {
                         continue;
                     }
 
@@ -590,12 +640,12 @@ public class RoomGenerationController : MonoBehaviour {
 
                     //Remove floating tiles and dead ends
                     if (NeighbourCount(new Vector2Int(x, y)) <= 1 &&
-                            (grid[x, y].tileType == Tile.TileType.DOORWAY || grid[x, y].tileType == Tile.TileType.CORRIDOR)) {
+                        (grid[x, y].tileType == Tile.TileType.DOORWAY || grid[x, y].tileType == Tile.TileType.CORRIDOR)) {
                         RemoveTile(new Vector2Int(x, y), newGrid);
                     }
 
                     if (x - 2 < 0 || x + 2 >= grid.GetLength(0) ||
-                            y - 2 < 0 || y + 2 >= grid.GetLength(1)) {
+                        y - 2 < 0 || y + 2 >= grid.GetLength(1)) {
                         continue;
                     }
 
@@ -604,12 +654,15 @@ public class RoomGenerationController : MonoBehaviour {
                         if ((grid[x - 2, y].taken == 1 && grid[x - 2, y].tileType == Tile.TileType.DOORWAY)) {
                             RemoveTile(new Vector2Int(x - 2, y), newGrid);
                         }
+
                         if ((grid[x + 2, y].taken == 1 && grid[x + 2, y].tileType == Tile.TileType.DOORWAY)) {
                             RemoveTile(new Vector2Int(x + 2, y), newGrid);
                         }
+
                         if ((grid[x, y - 2].taken == 1 && grid[x, y - 2].tileType == Tile.TileType.DOORWAY)) {
                             RemoveTile(new Vector2Int(x, y - 2), newGrid);
                         }
+
                         if ((grid[x, y + 2].taken == 1 && grid[x, y + 2].tileType == Tile.TileType.DOORWAY)) {
                             RemoveTile(new Vector2Int(x, y + 2), newGrid);
                         }
@@ -638,14 +691,20 @@ public class RoomGenerationController : MonoBehaviour {
         newGrid[pos.x, pos.y].tileType = Tile.TileType.NONE;
         GameObject g = newGrid[pos.x, pos.y].obj;
         newGrid[pos.x, pos.y].obj = null;
-        if(g != null) DestroyImmediate(g);
+        if (g != null) DestroyImmediate(g);
     }
 
     private void ScaleMap(float factor) {
         transform.transform.localScale = new Vector3(factor, factor, factor);
+
+        //since we scale up the map, we also have to scale up the spawnPositions with the same factor
+        for (int i = 0; i < eligibleSpawnPositions.Count; i++) {
+            eligibleSpawnPositions[i] *= factor;
+        }
     }
 
     #region FLOOD_FILL
+
     private Tile[,] highestGrid = null;
     private int highestSection = 0;
 
@@ -679,7 +738,8 @@ public class RoomGenerationController : MonoBehaviour {
         q.Push(new Vector2Int(x, y));
 
         //create current grid
-        Tile[,] currentGrid = new Tile[grid.GetLength(0), grid.GetLength(1)]; ;
+        Tile[,] currentGrid = new Tile[grid.GetLength(0), grid.GetLength(1)];
+        ;
         int currentSection = 0;
         for (int i = 0; i < grid.GetLength(0); i++) {
             for (int k = 0; k < grid.GetLength(1); k++) {
@@ -693,14 +753,14 @@ public class RoomGenerationController : MonoBehaviour {
             grid[n.x, n.y].filled = true;
             currentSection++;
             currentGrid[n.x, n.y] = new Tile(grid[n.x, n.y].taken, null, grid[n.x, n.y].tileType, grid[n.x, n.y].filled);
-            if(n.x + 1 < grid.GetLength(0)) q.Push(new Vector2Int(n.x + 1, n.y));
+            if (n.x + 1 < grid.GetLength(0)) q.Push(new Vector2Int(n.x + 1, n.y));
             if (n.x - 1 > 0) q.Push(new Vector2Int(n.x - 1, n.y));
             if (n.y + 1 < grid.GetLength(1)) q.Push(new Vector2Int(n.x, n.y + 1));
             if (n.y - 1 > 0) q.Push(new Vector2Int(n.x, n.y - 1));
         }
 
         //replace the highestSection with the new highest
-        if(currentSection > highestSection) {
+        if (currentSection > highestSection) {
             highestSection = currentSection;
             for (int i = 0; i < currentGrid.GetLength(0); i++) {
                 for (int k = 0; k < currentGrid.GetLength(1); k++) {
@@ -708,8 +768,9 @@ public class RoomGenerationController : MonoBehaviour {
                 }
             }
         }
+
         return;
     }
+
     #endregion
 }
-
