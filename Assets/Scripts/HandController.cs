1abf4212a64b4fcf45416deb91f4b91f9612ad44
Carl Eriksson <carl.erikssonn@hotmail.com>
2024-04-27 18:40:03 +0200

    Add pickupObjects and gun switching

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -1,40 +1,56 @@
 using System;
 using UnityEngine;
+using Logger = erikssonn.Logger;
 
 public class HandController : MonoBehaviour {
     [SerializeField] private GunController[] guns = null;
+    [SerializeField] private GameObject[] gunPrefabs = null;
 
     private int currentIndex = 0;
 
     private void Start() {
-        SetGun(0);
+        SetGun(0, null);
     }
 
-    private void Update() {
-        float scroll = Input.GetAxisRaw("Mouse ScrollWheel");
-        int oldIndex = currentIndex;
+    // private void Update() {
+    //     float scroll = Input.GetAxisRaw("Mouse ScrollWheel");
+    //     int oldIndex = currentIndex;
+    //
+    //     if (scroll > 0.0f) {
+    //         currentIndex++;
+    //         if (currentIndex > guns.Length - 1)
+    //             currentIndex = guns.Length - 1;
+    //     } else if (scroll < 0.0f) {
+    //         currentIndex--;
+    //         if (currentIndex < 0)
+    //             currentIndex = 0;
+    //     }
+    //
+    //     if (oldIndex == currentIndex)
+    //         return;
+    //     SetGun(currentIndex);
+    // }
 
-        if (scroll > 0.0f) {
-            currentIndex++;
-            if (currentIndex > guns.Length - 1)
-                currentIndex = guns.Length - 1;
-        } else if (scroll < 0.0f) {
-            currentIndex--;
-            if (currentIndex < 0)
-                currentIndex = 0;
-        }
-
-        if (oldIndex == currentIndex)
-            return;
-        SetGun(currentIndex);
-    }
-
-    private void SetGun(int index) {
+    public void SetGun(int index, GameObject refGun) {
         foreach (GunController gun in guns) {
             gun.gameObject.SetActive(false);
         }
 
+        if (index == currentIndex) {
+            return;
+        }
+        
+        DropGun();
+        if (refGun != null) {
+            Destroy(refGun);
+        }
         guns[index].gameObject.SetActive(true);
         guns[index].UpdateGunUi();
+        currentIndex = index;
+    }
+
+    private void DropGun() {
+        GameObject droppedGun = Instantiate(gunPrefabs[currentIndex]);
+        droppedGun.transform.SetPositionAndRotation(transform.position - new Vector3(0, 1, 0), Quaternion.identity);
     }
 }
---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using Logger = erikssonn.Logger;
+
+public class PickupObject : MonoBehaviour {
+	[SerializeField] private int index = 0;
+
+	[Header("Bobbing settings: ")]
+	[SerializeField] private float bobSpeed = 0.0f;
+	[SerializeField] private float bobStrength = 0.0f;
+	[SerializeField] private GameObject model = null;
+
+	private Vector3 origin = Vector3.zero;
+	private Vector3 dest = Vector3.zero;
+	private float time = -1.0f;
+
+	private void Start() {
+		origin = model.transform.localPosition;
+	}
+
+	private void LateUpdate() {
+		PickupBobbing();
+	}
+
+	private void PickupBobbing() {
+		time = Mathf.PingPong(Time.time * bobSpeed, 2.0f) - 1.0f;
+		dest = new Vector3(0.0f, time * bobStrength, 0.0f);
+
+		model.transform.localPosition = origin + dest;
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (!other.CompareTag("Player")) {
+			return;
+		}
+
+		other.GetComponentInChildren<HandController>().SetGun(index, gameObject);
+	}
+}
